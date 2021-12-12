a4195da1e9b5b23e97b9e3b367f4d0a2530f0781
Blizzard109 <ruben.steenbrink@student.hu.nl>
2021-12-12 16:53:17 +0100

    Made VervangGehelenString to complete assignment.

---
+++ huOOP/src/main/java/rs/weerk4/practicum7/VervangGehelenString.java
@@ -0,0 +1,13 @@
+package rs.weerk4.practicum7;
+
+public class VervangGehelenString implements OpmaakProces {
+    public VervangGehelenString(){}
+
+    @Override
+    public String maakOp(String input) {
+        String s = input;
+        s = s.replaceAll(s,"MOST SECRET");
+        System.out.println(s);
+        return s;
+    }
+}
